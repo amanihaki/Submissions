18aa032983e0f23800977fdba6f9922df7f1294d
Your Name <your.email@example.com>
2019-05-21 22:31:05 +0300

    edited basic2/step3

--- Exercises/Javascript-Basics-04/questions.js
+++ Exercises/Javascript-Basics-04/questions.js
@@ -1,30 +1,52 @@
 
-const stringSize = (text) => {
-
+const stringSize = () => {
+    const n  = "This text has a certain number of characters";
+    count = 0 ;
+    for(var i = 0 ; i < n.length ; i++)
+    {
+        count++ ;
+    }
+     return count  ;  
 }
 const replaceCharacterE = (text) => {
-
+    text = "This text has a certain number of characters";
+    var x = text.replace(/e/g, " ");
+    return x ;
 }
 const concatString = (text1, text2) => {
+    text1 = 'el Javascript shi ' ;
+    text2 = 'ra2e3' ;
+    var text3 = text1 + text2 ;
 
+    return text3 ;jpg
 }
 const showChar5 = (text) => {
-
+   // text = 'TDD kicks ass' ;
+    var res = text.substring(4,5);
+    return res ;
 }
 const showChar9 = (text) => {
-
+    //text = 'But it is really annoying to write' ;
+    var res = text.substring(0,9);
+    return res ;
 }
 const toCapitals = (text) => {
+    //text = 'Another useful function' ;
+    var res = text.toUpperCase();
+    return res ;
 
 }
 const toLowerCase = (text) => {
+    var res = text.toLowerCase();
+    return res ;
 
 }
 const removeSpaces = (text) => {
-
+    var res = text.trim();
+    return res ;
 }
 const IsString = (text) => {
-
+    
 }
 
 const getExtension = (text) => {
