de2a7b94e991e826b5a54c38529e8c155390bb97
Your Name <your.email@example.com>
2019-06-11 23:28:55 +0300

    Step2

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,1 +1,19 @@
- 
+const express = require('express');
+const port = 3000;
+const app = express();
+
+
+app.listen(port, function () {
+  console.log("Server is running on "+ port +" port");
+});
+
+app.get('/', function (req, res) {
+  res.send('<h1>Ok</h1>')
+})
+
+// const express = require('express');
+// var app = express();
+// app.get('/', (req, res) => {
+//     res.send('OK')
+// });
+// app.listen(process.env.PORT || 3000)
