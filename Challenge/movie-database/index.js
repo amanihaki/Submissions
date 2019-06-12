96edb4d2011587031a7708989318102d1d6bed9b
Your Name <your.email@example.com>
2019-06-12 12:05:04 +0300

    Step4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -7,18 +7,45 @@ app.listen(port, function () {
   console.log("Server is running on "+ port +" port");
 });
 
+
+
 app.get('/', function (req, res) {
   res.send('ok')
 }) 
+
+
 app.get('/test', function (req, res) {
   res.send({status:200, message:"ok"})
 }) 
+
+
 app.get('/time', function (req, res) {
   	
 var today = new Date();
 var TIME = today.getHours() + ":" + today.getSeconds();
   res.send({status:200, message:TIME})
 }) 
+
+app.get('/hello/:ID', function (req, res) {
+     var id = req.params.ID
+  res.send({status:200, message:"Hello" + id})
+}) ;
+
+
+app.get('/search', function (req, res) {
+  var search = req.query.s
+  if(search !== undefined && search !== ""){
+    res.send({status:200, message:"ok", data:search})
+    
+  }
+else{
+  
+  res.send({status:500, error:true, message:"you have to provide a search"})
+  }
+})
+ 
+
+
 
 // const express = require('express');
 // var app = express();
